8cd04821dfd1b2c8a9f503d744b5345d0a20a500
Tom Griffin <t.a.griffin@gmail.com>
2020-09-18 10:29:59 +1200

    Create function to make 4th dot visiblet

--- classes.js
+++ classes.js
@@ -8,6 +8,7 @@ function start () {
   one()
   two()
   three()
+  makeVisible()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -31,3 +32,8 @@ function three() {
   three.classList.add('red')
 }
 // CREATE FUNCTION makeVisible HERE
+function makeVisible() {
+  var makeVisible = document.getElementsByClassName('invisible')[0]
+  console.log(makeVisible)
+  makeVisible.classList.add('visible')
+}
