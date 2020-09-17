38b85f443037f05023dee1077ab92f603e752c15
Tom Griffin <t.a.griffin@gmail.com>
2020-09-18 10:16:36 +1200

    Create function to apply red class to 3rd dot

--- classes.js
+++ classes.js
@@ -7,6 +7,7 @@ function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
   one()
   two()
+  three()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -25,5 +26,8 @@ var two = document.getElementById('two')
 two.classList.add('green')
 }
 // CREATE FUNCTION three HERE
-
+function three() {
+  var three = document.getElementById('three')
+  three.classList.add('red')
+}
 // CREATE FUNCTION makeVisible HERE
